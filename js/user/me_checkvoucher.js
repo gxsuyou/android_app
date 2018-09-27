0923697b5d91f17b635929ee33f98ca7c16e81dc
gxsuyou <gxsuyou@126.com>
2018-09-27 18:21:22 +0800

    ===

--- js/user/me_checkvoucher.js
+++ js/user/me_checkvoucher.js
@@ -1,19 +1,53 @@
+var tu_id = null;
 mui.plusReady(function() {
 
-	var mask = mui.createMask(function(){
-//		return false
-	});//callback为用户点击蒙版时自动执行的回调；
-     
-    $("body").on("tap",".me_tochar",function(){
-    	//		var playerId=$(".playerId").val()
-//		var phoneNum=$(".phoneNum").val()
-//		var playDir=$(".playDir").val()
-//		alert(playerId)
-    	mask.show()
-    	$(".me_downcontents").css("display","block")
-    })
-    $("body").on("tap",".me_downclose",function(){
-    	mask.close();//关闭遮罩
-    	$(".me_downcontents").css("display","none")
-    })
+	var n = plus.webview.currentWebview();
+	tu_id = n.tu_id
+
+	$(".me_voulan img").attr("src", n.icon_href)
+	$(".me_voulan span").text(n.game_name)
+	var mask = mui.createMask(function() {
+		//		return false
+	}); //callback为用户点击蒙版时自动执行的回调；
+
+	$("body").on("tap", ".me_tochar", function() {	
+		var playerId = $(".playerId").val()
+		var phoneNum = $(".phoneNum").val()
+		var playDir = $(".playDir").val()
+		if(playerId==""||phoneNum==""){
+			mui.toast("请输入必要信息")
+			return false;
+		}
+		if(phoneNum.length>15){
+			mui.toast("请输入准确号码")
+			return false;
+		}
+
+		$.ajax({
+			type: "post",
+			url: config.data + "game/getUseTicket",
+			async: true,
+			data: {
+				uid: userId,
+				tu_id:tu_id,
+				game_user:playerId,
+				game_area:playDir,
+				tel:phoneNum
+			},
+			success: function(data){
+				if(data.state==1){
+					mui.toast("提交，正在审核中。")
+				}
+			}
+		})
+
+		return false;
+		mask.show()
+		$(".me_downcontents").css("display", "block")
+	})
+	$("body").on("tap", ".me_downclose", function() {
+
+		mask.close(); //关闭遮罩
+		$(".me_downcontents").css("display", "none")
+	})
 })
--- js/user/me_pocket.js
+++ js/user/me_pocket.js
@@ -1,10 +1,50 @@
-$(function() {
+//$(function() {
 	mui.plusReady(function() {
+		
+		
+
+		$.ajax({
+			type: "get",
+			url: config.data+"user/getUserMyCoin",
+			async: true,
+			data: {
+				uid:userId
+			},
+			success: function(data){
+				alert(JSON.stringify(data))
+//				if(data.state==1){
+//					mui.toast("提交，正在审核中。")
+//				}
+			}
+		})
+
+
+		
 		$("body").on("tap", ".topheadright", function() {
 			alert(1)
 		})
-		$("body").on("tap",".me_goldInforight",function(){
-			alert(1)
+		$("body").on("tap", ".me_goldInforight", function() {
+			mui.openWindow({
+			   url:"me_withdraw.html",
+			   id:"me_withdraw.html",
+			})
+
 		})
+		$("body").on("tap", ".voucher_header", function() {
+
+			var no = $(this).siblings(".voucher_getgoldcontent").css("display")
+
+			if(no == "none") {
+				$(this).find("a").removeClass("mui-icon-arrowdown")
+				$(this).find("a").addClass("mui-icon-arrowup")
+				$(this).siblings(".voucher_getgoldcontent").css("display", "flex")
+			}else{
+				$(this).find("a").removeClass("mui-icon-arrowup")
+				$(this).find("a").addClass("mui-icon-arrowdown")
+				$(this).siblings(".voucher_getgoldcontent").css("display", "none")
+			}
+
+		})
+
 	})
-})
+//})
