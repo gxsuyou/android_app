64e6c584c017f6e61bf1fc8c9babb15b9c660e38
gxsuyou <gxsuyou@126.com>
2018-09-28 18:05:26 +0800

    ====

--- js/user/me_pocket.js
+++ js/user/me_pocket.js
@@ -1,50 +1,56 @@
-//$(function() {
-	mui.plusReady(function() {
-		
-		
 
-		$.ajax({
-			type: "get",
-			url: config.data+"user/getUserMyCoin",
-			async: true,
-			data: {
-				uid:userId
-			},
-			success: function(data){
-				alert(JSON.stringify(data))
-//				if(data.state==1){
-//					mui.toast("提交，正在审核中。")
-//				}
+var coin=0;
+mui.plusReady(function() {
+
+	$.ajax({
+		type: "get",
+		url: config.data + "users/getUserMyCoin",
+		async: true,
+		data: {
+			uid: userId
+		},
+		success: function(data) {
+			$(".me_goldInfoLeft>div:last-child").html(data.coin + "<span>币</span>")
+			coin=data.coin;
+		}
+	})
+	
+	
+	
+	
+
+	$("body").on("tap", ".topheadright", function() {
+		mui.openWindow({
+			url:"me_dealrecord.html",
+			id:"me_dealrecord.html",
+		})
+	})
+	
+	
+	$("body").on("tap", ".me_goldInforight", function() {
+		mui.openWindow({
+			url: "me_withdraw.html",
+			id: "me_withdraw.html",
+			extras:{
+				coin:coin
 			}
-		})
-
-
-		
-		$("body").on("tap", ".topheadright", function() {
-			alert(1)
-		})
-		$("body").on("tap", ".me_goldInforight", function() {
-			mui.openWindow({
-			   url:"me_withdraw.html",
-			   id:"me_withdraw.html",
-			})
-
-		})
-		$("body").on("tap", ".voucher_header", function() {
-
-			var no = $(this).siblings(".voucher_getgoldcontent").css("display")
-
-			if(no == "none") {
-				$(this).find("a").removeClass("mui-icon-arrowdown")
-				$(this).find("a").addClass("mui-icon-arrowup")
-				$(this).siblings(".voucher_getgoldcontent").css("display", "flex")
-			}else{
-				$(this).find("a").removeClass("mui-icon-arrowup")
-				$(this).find("a").addClass("mui-icon-arrowdown")
-				$(this).siblings(".voucher_getgoldcontent").css("display", "none")
-			}
-
 		})
 
 	})
-//})
+	$("body").on("tap", ".voucher_header", function() {
+
+		var no = $(this).siblings(".voucher_getgoldcontent").css("display")
+
+		if(no == "none") {
+			$(this).find("a").removeClass("mui-icon-arrowdown")
+			$(this).find("a").addClass("mui-icon-arrowup")
+			$(this).siblings(".voucher_getgoldcontent").css("display", "flex")
+		} else {
+			$(this).find("a").removeClass("mui-icon-arrowup")
+			$(this).find("a").addClass("mui-icon-arrowdown")
+			$(this).siblings(".voucher_getgoldcontent").css("display", "none")
+		}
+
+	})
+
+})
